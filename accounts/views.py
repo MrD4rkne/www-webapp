7bc984c15100fe329f5643b8ab5b689a2c088413
Marcin Szopa <marcinsz.pv@gmail.com>
2025-04-24 20:44:56 +0200

    chore: add missing files

---
+++ accounts/views.py
@@ -0,0 +1,23 @@
+
+# views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Authenticate and log in the user
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        else:
+            # Handle form errors
+            print(form.errors)
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
