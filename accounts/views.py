80a25092686927cd81bae6fff0da9160be929ce6
Marcin Szopa <marcinsz.pv@gmail.com>
2025-04-24 20:52:23 +0200

    feat: display errors in forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,3 @@
-
-# views.py
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from .forms import SignUpForm
@@ -8,7 +6,6 @@ def signup_view(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
             # Authenticate and log in the user
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
