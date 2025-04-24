7e705703bdd969be21cad929524ab7df6e6511b4
Marcin Szopa <marcinsz.pv@gmail.com>
2025-04-24 20:41:50 +0200

    feat: add signup, hide admin button if user does not have access

--- www-project/urls.py
+++ www-project/urls.py
@@ -6,8 +6,7 @@ from . import views
 
 urlpatterns = [
     path("images/", include("images.urls")),
-    path("routes/", include("routes.urls")),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
     path("admin/", admin.site.urls),
-    path("", views.get_index, name="get_index")
+    path("", views.get_index, name="home")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
