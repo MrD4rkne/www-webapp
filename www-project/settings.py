40ba0a846620318b0711473ab58df16310acf2ed
Marcin Szopa <marcinsz.pv@gmail.com>
2025-05-19 18:50:43 +0200

    chore: change default value of db host for running migrations without changing the server

--- www-project/settings.py
+++ www-project/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', 'postgres'),
         'USER': os.getenv('DB_USER', 'postgres'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'hunter2'),
-        'HOST': os.getenv('DB_HOST', 'db'),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
