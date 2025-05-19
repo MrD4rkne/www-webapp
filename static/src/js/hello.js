d6dca4f6233a6d9601b59f1b054ff7a10a004dbc
Marcin Szopa <marcinsz.pv@gmail.com>
2025-05-19 19:09:37 +0200

    feat: add TypeScript support and update build process for CSS

---
+++ static/src/js/hello.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Hello, world!");
+// This is a simple TypeScript file that logs "Hello, world!" to the console.
+// It serves as a basic example of how to set up a TypeScript file in a project.
+//# sourceMappingURL=hello.js.map
---
+++ static/src/js/hello.ts
@@ -0,0 +1,4 @@
+﻿console.log("Hello, world!");
+
+// This is a simple TypeScript file that logs "Hello, world!" to the console.
+// It serves as a basic example of how to set up a TypeScript file in a project.
